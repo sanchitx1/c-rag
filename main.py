9eb57afdfde3cbb4421a0c97e71fca501e80ed35
sanchitx1 <sanchity2005@gmail.com>
2026-06-14 01:43:41 +0530

    new type init --uv

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-import chromadb
-chroma_client = chromadb.Client()
+from docling.document_converter import DocumentConverter
+
+def main():
+    converter = DocumentConverter()
+    result = converter.convert("sanchitx1-resume.pdf")
+    text = result.document.export_to_text()
+    chunk_size = 1000
+    overlap = 200
+    chunks = []
+    
+    for i in range(0, len(text), chunk_size - overlap):
+        chunks.append(text[i:i + chunk_size])
+    
+    print(f"Created {len(chunks)} chunks\n")
+    
+    for idx, chunk in enumerate(chunks, start=1):
+        print(f"--- CHUNK {idx} ---")
+        print(chunk)
+        print("\n")
+
+if __name__ == "__main__":
+    main()
